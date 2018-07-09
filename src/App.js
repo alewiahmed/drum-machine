9c6c80da9aad2a51c646d601a4d9a189e1dbcf90
Alewi Ahmed <alewimail@gmail.com>
2018-07-09 17:33:38 +0300

    created the layout of the drum.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App">Drum Machine project</div>;
+    return (
+      <div className="App">
+        <div id="drum-machine">
+          <div className="drum-container">
+            <div className="drum-pad">pad 1</div>
+            <div className="drum-pad">pad 2</div>
+            <div className="drum-pad">pad 3</div>
+            <div className="drum-pad">pad 4</div>
+            <div className="drum-pad">pad 5</div>
+            <div className="drum-pad">pad 6</div>
+            <div className="drum-pad">pad 7</div>
+            <div className="drum-pad">pad 8</div>
+            <div className="drum-pad">pad 9</div>
+          </div>
+          <div className="controllers">
+            <div>Power</div>
+            <div id="display">display</div>
+            <div>Volume</div>
+            <div>Bank</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
