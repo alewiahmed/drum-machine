defc540f10be205a7462d690d1276a95de2bab30
Alewi Ahmed <alewimail@gmail.com>
2018-07-09 19:23:50 +0300

    Added the sound play feature.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,171 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+import allSounds from './sounds';
+
+const soundsOne = [
+  {
+    key: 'Q',
+    id: 'heater-1',
+    name: 'Heater 1',
+    src: allSounds.heaterOne
+  },
+  {
+    key: 'W',
+    id: 'heater-2',
+    name: 'Heater 2',
+    src: allSounds.heaterTwo
+  },
+  {
+    key: 'E',
+    id: 'heater-3',
+    name: 'Heater 3',
+    src: allSounds.heaterThree
+  },
+  {
+    key: 'A',
+    id: 'heater-4',
+    name: 'Heater 4',
+    src: allSounds.heaterFour
+  },
+  {
+    key: 'S',
+    id: 'clap',
+    name: 'Clap',
+    src: allSounds.clap
+  },
+  {
+    key: 'D',
+    id: 'open-hh',
+    name: 'Open HH',
+    src: allSounds.openHH
+  },
+  {
+    key: 'Z',
+    id: 'kick-n-hat',
+    name: `kick n'Hat`,
+    src: allSounds.kickNHat
+  },
+  {
+    key: 'X',
+    id: 'kick',
+    name: 'Kick',
+    src: allSounds.kick
+  },
+  {
+    key: 'C',
+    id: 'closed-hh',
+    name: 'Closed HH',
+    src: allSounds.closedHH
+  }
+];
+
+const soundsTwo = [
+  {
+    key: 'Q',
+    id: 'chord-1',
+    name: 'Chord 1',
+    src: allSounds.chordOne
+  },
+  {
+    key: 'W',
+    id: 'chord-2',
+    name: 'Chord 2',
+    src: allSounds.chordTwo
+  },
+  {
+    key: 'E',
+    id: 'chord-3',
+    name: 'Chord 3',
+    src: allSounds.chordThree
+  },
+  {
+    key: 'A',
+    id: 'shaker',
+    name: 'Shaker',
+    src: allSounds.shaker
+  },
+  {
+    key: 'S',
+    id: 'open-hh',
+    name: 'Open HH',
+    src: allSounds.dryOpenHH
+  },
+  {
+    key: 'D',
+    id: 'closed-hh',
+    name: 'Closed HH',
+    src: allSounds.dryClosedHH
+  },
+  {
+    key: 'Z',
+    id: 'punchy-kick',
+    name: 'Punchy Kick',
+    src: allSounds.punchyKick
+  },
+  {
+    key: 'X',
+    id: 'side-stick',
+    name: 'Side Stick',
+    src: allSounds.sideStick
+  },
+  {
+    key: 'C',
+    id: 'snare',
+    name: 'Snare',
+    src: allSounds.snare
+  }
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      kitType: 0,
+      currentPad: null
+    };
+    this.audios = [];
+  }
+
+  showDrumPads = () => {
+    let { kitType } = this.state;
+    let sounds = kitType === 0 ? soundsOne : soundsTwo;
+    return sounds.map((sound, index) => {
+      return (
+        <div
+          key={index}
+          id={sound.id}
+          className="drum-pad"
+          onClick={() => this.playSound(sound.key)}
+        >
+          {sound.key}
+          <audio className="clip" id={sound.key}>
+            <source src={sound.src} type="audio/mpeg" />
+          </audio>
+        </div>
+      );
+    });
+  };
+
+  display = () => {
+    let { currentPad } = this.state;
+    return currentPad === null ? '' : currentPad;
+  };
+
+  playSound = id => {
+    let audio = document.getElementById(id);
+    audio.currentTime = 0;
+    audio.play();
+  };
+
   render() {
     return (
       <div className="App">
         <div id="drum-machine">
-          <div className="drum-container">
-            <div className="drum-pad">pad 1</div>
-            <div className="drum-pad">pad 2</div>
-            <div className="drum-pad">pad 3</div>
-            <div className="drum-pad">pad 4</div>
-            <div className="drum-pad">pad 5</div>
-            <div className="drum-pad">pad 6</div>
-            <div className="drum-pad">pad 7</div>
-            <div className="drum-pad">pad 8</div>
-            <div className="drum-pad">pad 9</div>
-          </div>
+          <div className="drum-container">{this.showDrumPads()}</div>
           <div className="controllers">
             <div>Power</div>
-            <div id="display">display</div>
+            <p id="display">{this.display()}</p>
             <div>Volume</div>
             <div>Bank</div>
           </div>
---
+++ src/sounds/index.js
@@ -0,0 +1,39 @@
+import clap from './Clap.mp3';
+import kick from './Kick.mp3';
+import Snare from './Snare.mp3';
+import shaker from './Shaker.mp3';
+import openHH from './Dsc_Ohh.mp3';
+import closedHH from './Cev_HH.mp3';
+import chordOne from './Chord_1.mp3';
+import chordTwo from './Chord_2.mp3';
+import dryOpenHH from './Dry_Ohh.mp3';
+import chordThree from './Chord_3.mp3';
+import dryClosedHH from './Dry_HH.mp3';
+import heaterOne from './Heater-1.mp3';
+import heaterTwo from './Heater-2.mp3';
+import kickNHat from './Kick_n_Hat.mp3';
+import heaterFour from './Heater-4.mp3';
+import sideStick from './side_stick.mp3';
+import heaterThree from './Heater-3.mp3';
+import punchyKick from './punchy_kick.mp3';
+
+export default {
+  clap,
+  kick,
+  Snare,
+  openHH,
+  shaker,
+  chordOne,
+  closedHH,
+  kickNHat,
+  chordTwo,
+  dryOpenHH,
+  heaterOne,
+  sideStick,
+  heaterTwo,
+  punchyKick,
+  chordThree,
+  heaterFour,
+  heaterThree,
+  dryClosedHH
+};
